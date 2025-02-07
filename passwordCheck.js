0ad0aa55b5e71e7547c655852240d692296d3da7
Tetiana Gusar <tetiana_gusar@epam.com>
2025-02-08 00:06:57 +0200

    Add cartTotal.js and save changes to passwordCheck.js

--- passwordCheck.js
+++ passwordCheck.js
@@ -17,9 +17,5 @@ console.log(checkPasswordStrength("password"));
 console.log(checkPasswordStrength("Password123")); 
 console.log(checkPasswordStrength("12345678")); 
 console.log(checkPasswordStrength("WeakPass1")); 
-<<<<<<< HEAD
 console.log(checkPasswordStrength("STRONG123")); 
-=======
-console.log(checkPasswordStrength("STRONG123"));
->>>>>>> 32ec7e3661c7c736753751dc14f60a6c443f75d9
 
